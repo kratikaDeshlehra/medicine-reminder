b2337582edff7a4e23aad009813bceaa8571ce4d
kratika <kratikadeshlehra@gmail.com>
2025-05-17 16:49:11 +0530

    created Biometric authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,17 @@
-import {View, Text,StyleSheet, Animated} from 'react-native'
-import {Ionicons} from '@expo/vector-icons'
-import { useEffect,useRef, } from 'react'
+import { View, Text, StyleSheet, Animated } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { useEffect, useRef, } from 'react'
 import { useRouter } from 'expo-router';
 
 
 
-export default function SplashScreen(){
+export default function SplashScreen() {
 
   const router = useRouter();
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const scaleAnim = useRef(new Animated.Value(0.5)).current;
 
-    useEffect(() => {
+  useEffect(() => {
     Animated.parallel([
       Animated.timing(fadeAnim, {
         toValue: 1,
@@ -26,35 +26,35 @@ export default function SplashScreen(){
       }),
     ]).start();
 
-       const timer = setTimeout(() => {
+    const timer = setTimeout(() => {
       router.replace("/auth");
     }, 2000);
 
     return () => clearTimeout(timer);
-    
+
   }, []);
-    return (
-        <View style={styles.container}>
-            <Animated.View style={[styles.iconContainer,{
-                opacity: fadeAnim,
-                transform:[{scale:scaleAnim}],
-            }]}>
-    <Ionicons name="medical" size={100} color="white" />
-          <Text style={styles.appName}>SehatSathi</Text>
-            </Animated.View>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.iconContainer, {
+        opacity: fadeAnim,
+        transform: [{ scale: scaleAnim }],
+      }]}>
+        <Ionicons name="medical" size={100} color="white" />
+        <Text style={styles.appName}>SehatSathi</Text>
+      </Animated.View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#4CAF50",
+    backgroundColor: "#4CAF50", // green background
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center",  //center everything
   },
   iconContainer: {
-     alignItems:'center'
+    alignItems: 'center'
   },
   appName: {
     color: "white",
